fcf23d168029e4305a89b254efa9184296bbaa4c
cham-s <cc.attou@gmail.com>
2020-04-17 21:45:36 +0200

    adding rethrows to function

--- mymap.swift
+++ mymap.swift
@@ -1,12 +1,12 @@
 import Foundation
 
 extension Collection {
-    func myMap<T>(transform: (Self.Element) -> T) -> [T] {
+    func myMap<T>(transform: (Self.Element) throws -> T) rethrows -> [T] {
         var array: [T] = []
         array.reserveCapacity(count)
 
         for element in self {
-            array.append(transform(element))
+            array.append(try transform(element))
         }
         return array
     }
