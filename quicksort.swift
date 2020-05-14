1aeed37f49caa19bc55adb53ebd226632034a982
cham-s <cc.attou@gmail.com>
2020-05-14 21:00:02 +0200

    working on quicksort

---
+++ quicksort.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+extension Collection {
+    var indexMiddle: Index {
+        self.index(self.startIndex, offsetBy: count / 2)
+    }
+}
+
+extension Array where Self.Element : Comparable {
+    mutating func quicksorted() {
+        var arrayCopy = self
+        
+        
+        func quickSort() {
+            var middleIndex =
+            for currentIndex in arr {
+                if currentIndex > middleIndex {
+                    
+                }
+            }
+        }
+        
+    }
+}
+
