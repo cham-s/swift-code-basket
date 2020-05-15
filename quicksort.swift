7c4e79b094c633ae2932d022331f1d5314566b31
cham-s <cc.attou@gmail.com>
2020-05-15 20:49:10 +0200

    quicksort changed but unfinished

--- quicksort.swift
+++ quicksort.swift
@@ -1,25 +1,28 @@
 import Foundation
 
-extension Collection {
-    var indexMiddle: Index {
-        self.index(self.startIndex, offsetBy: count / 2)
-    }
-}
-
-extension Array where Self.Element : Comparable {
-    mutating func quicksorted() {
+extension Array where Self.Element: Comparable {
+    func quicksorted() -> [Self.Element] {
         var arrayCopy = self
         
-        
-        func quickSort() {
-            var middleIndex =
-            for currentIndex in arr {
-                if currentIndex > middleIndex {
-                    
+        func quickSort(range: Range<Int>) {
+            guard (range.upperBound + 1) - range.lowerBound > 1 else { return }
+            var pivotIndex = range.lowerBound + range.upperBound / 2
+            for currentIndex in arrayCopy.indices {
+                if currentIndex > pivotIndex {
+                    let tmpElement = arrayCopy[pivotIndex]
+                    arrayCopy[pivotIndex] = arrayCopy[currentIndex]
+                    arrayCopy[currentIndex] = tmpElement
+                    pivotIndex = currentIndex
                 }
             }
+            // left side of the pivot
+            quickSort(range: 0..<pivotIndex+1)
+            // right side of the pivot
+            quickSort(range: pivotIndex..<count)
         }
         
+        quickSort(range: 0..<count)
+        return arrayCopy
     }
 }
 
